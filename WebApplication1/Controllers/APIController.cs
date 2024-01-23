7b2056f0ccf6ddaaa7f5ef19c0682539cc7fd5d5
CrownChen <smilett07@gmail.com>
2024-01-23 20:56:56 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/APIController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class APIController : Controller
+    {
+        private readonly MyDBContext _dbContext;
+
+        public APIController(MyDBContext context)
+
+        {
+
+            _dbContext = context;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            var cities = _dbContext.Addresses.Select(a=>a.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
